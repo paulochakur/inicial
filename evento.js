b11adf3d88a24429c3e261e6a02a4a5a9c99bb37
paulochakur <paulosergiosaraivachakur@gmail.com>
2023-06-24 14:58:01 -0300

    arquivo js

--- evento.js
+++ evento.js
@@ -440,13 +440,12 @@ function iniSys(){
     // -------- finaliza rotina inicial
 
     // ... situação inicial de Console
-    print(' CONSOLES')
     el('console').style.left = '-3000px' ; el('proprBox').style.left = '-3000px' ; proprHab = 0
     el('console').style.zIndex = '100' ; proprHab = 0
     
     deslX = el('Corpo').getAttribute("deslX") ; deslY = el('Corpo').getAttribute("deslY")
-    window.scrollTo(deslX,deslY+200)
-    print(' FIM')
+    window.scrollTo(deslX,deslY)
+    print(' FIM   '+deslY)
     iniLoc()
 }
 // ----- inicio Sys
