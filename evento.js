0ae5b96bc3c594027d6f0cfe5c9873aed3d1aeec
paulochakur <paulosergiosaraivachakur@gmail.com>
2023-08-15 13:47:53 -0300

    arquivo js

--- evento.js
+++ evento.js
@@ -522,7 +522,7 @@ function iniSys(){
 
     // ...... ajusta mob
     Wh = window.innerHeight             ; Ww = window.innerWidth
-    Ah = window.screen.availHeight-130  ; Aw = window.screen.availWidth
+    Ah = window.screen.availHeight-120  ; Aw = window.screen.availWidth
     if(Aw<1000){mobFlag = 1 }
 
 
