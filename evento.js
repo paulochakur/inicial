9ae26599d5cae9392ff822231bf80525b6a3505f
paulochakur <paulosergiosaraivachakur@gmail.com>
2023-09-01 12:35:14 -0300

    arquivo js

--- evento.js
+++ evento.js
@@ -522,16 +522,17 @@ function iniSys(){
 
     if (mobFlag==1 && wF>400){
         el('Fundo').style.transform = "rotateZ(-90deg)"+" scaleX("+fatX+")"+" scaleY("+fatY+")"
-        el('Fundo').style.left  = (-delV-40)+'px'
-        el('Fundo').style.top   = (-delH   )+'px'
+        el('Fundo').style.left  = (-delV)+'px'
+        el('Fundo').style.top   = (-delH-40)+'px'
         scrTurn = 1
     }
     // ......[ajusta mob]
 
     document.activeElement.blur()
-}
 
-// ----- inicio Sys
+    // ....
+}
+// -----[inicio Sys]
 
 //  -------------- Trap de Eventos  -------------
 function eventTrap() {
