f960e1908792c91c445f39b3b4fa755dec449d5f
paulochakur <paulosergiosaraivachakur@gmail.com>
2023-11-28 11:26:00 -0300

    arquivo js

--- evento.js
+++ evento.js
@@ -607,7 +607,7 @@ function iniSys(){
     delH    = (hF-wF)/2 + (wF/2)*(1-fatX)
 
     if (mobFlag==1 && wF>400){
-        el('Fundo').style.transform = "rotateZ(-90deg)"+" scaleX("+fatX+")"+" scaleY("+fatY+")"
+        //el('Fundo').style.transform = "rotateZ(-90deg)"+" scaleX("+fatX+")"+" scaleY("+fatY+")"
         el('Fundo').style.left  = (-delV)+'px'
         el('Fundo').style.top   = (-delH)+'px'
         scrTurn = 1
