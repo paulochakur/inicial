de0c6fe1e035b792a4dd08e84401a6f019943abe
paulochakur <paulosergiosaraivachakur@gmail.com>
2023-12-28 17:38:27 -0300

    arquivo js

--- evento.js
+++ evento.js
@@ -608,10 +608,11 @@ function iniSys(){
     screenTurn  = el('Fundo').getAttribute('screenTurn')
     scrFitWAtt  = el('Fundo').getAttribute('scrFitW')
     rotF = 0 ; fatX = 1 ; fatY = 1 ; delV = 0 ; delH = 0 ; scrFitW = 0
-    print('  screenTurn:'+screenTurn)
+    
     if(mobFlag==1 && screenTurn=='true')  { scrTurn = 1 }
     if(mobFlag==1 && scrFitWAtt!='false') { scrFitW = 1 }
-    scrFitW = 1 ; scrTurn = 1
+    //scrFitW = 1 ; scrTurn = 1
+    print('  screenTurn:'+screenTurn+' scrTurn:'+scrTurn+' mobFlag:'+mobFlag)
     if (scrTurn==1){ 
         rotF    = -90
         if(scrFitW==1){ fatX    =  Ih/wF ;    fatY = fatX ; hF = Iw/fatX}
@@ -629,7 +630,7 @@ function iniSys(){
     el('Fundo').style.top       = (-delV)+'px'
     el('Fundo').style.height    = (hF)+'px'
     el('Fundo').style.width     = (wF)+'px'
-
+    if(mobFlag==1){ el('Fundo').style.backgroundColor = 'red' }
     // ......[ajusta mob]
 
     document.activeElement.blur()
